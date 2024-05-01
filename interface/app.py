8edd7b05229b58d662389fd1f1ac9a39eb0526d6
Khadarabad Kailash <127466868+kailash105@users.noreply.github.com>
2024-05-01 21:56:20 +0530

    Add files via upload

---
+++ interface/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def web():
+    return render_template('main.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
